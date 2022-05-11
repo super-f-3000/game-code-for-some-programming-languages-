ed0d4903ff6c4180e5bdfa796ed33e111f16260e
super-f-3000 <81982904+super-f-3000@users.noreply.github.com>
2022-05-11 08:05:48 -0400

    Update 2d.game.js

--- build/2d.game.js
+++ build/2d.game.js
@@ -1,1 +1,19 @@
-
+const languages = []
+const GAME = {
+  "Code_Version": "0.0.01",
+  "Vector2": class {
+    constructor () {
+      
+    }
+  }
+  "KeyEvent": class {
+    constructor(element, event, Function) {
+      element.addEventListener(event, Function);
+    }
+  },
+  "2dObject": class {
+    constructor() {
+      
+    }
+  }
+}
