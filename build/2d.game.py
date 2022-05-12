0959a44a84017c1ddf89425603bfe5df8c35e2d3
super-f-3000 <81982904+super-f-3000@users.noreply.github.com>
2022-05-12 09:05:20 -0400

    Update 2d.game.py

--- build/2d.game.py
+++ build/2d.game.py
@@ -10,3 +10,8 @@ class Vector2:
 class Object2D:
   def init(self):
     self.position = Vector2(0, 0)
+    self.rotation = Vector2(0, 0)
+    self.scale = Vector2(1, 1)
+    self.name = ""
+    self.userData = json.loads("{}")
+    
