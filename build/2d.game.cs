40285757cb21c7f454ae41ac5e05f94c59332dfd
super-f-3000 <81982904+super-f-3000@users.noreply.github.com>
2022-05-16 14:35:14 -0400

    Update 2d.game.cs

--- build/2d.game.cs
+++ build/2d.game.cs
@@ -3,6 +3,14 @@ using System.Drawing;
 
 namespace GAME
 {
+  public class UserData
+  {
+    bool IA;
+    public UserData ()
+    {
+      IA = false;
+    }
+  }
   public class Vector2
   {
     int x;
@@ -18,5 +26,12 @@ namespace GAME
     Vector2 position = new Vector2(0, 0);
     Vector2 rotation = new Vector2(0, 0);
     Vector2 scale = new Vector2(1, 1);
+    string name;
+    object[] userData = new object[];
+    object[] children = new object[];
+    public Object2D ()
+    {
+      name = "";
+    }
   }
 }
