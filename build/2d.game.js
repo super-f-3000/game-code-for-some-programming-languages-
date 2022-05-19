deee196ed403cd73f735901ebae264fe1c9789a0
super-f-3000 <81982904+super-f-3000@users.noreply.github.com>
2022-05-19 15:05:29 -0400

    Update 2d.game.js

--- build/2d.game.js
+++ build/2d.game.js
@@ -1,7 +1,13 @@
 class dialogLine {
   constructor(parameters) {
     if (typeof parameters === "object") {
-      
+      if (parameters.title) {
+        this.title = parameters.title;
+      }else{
+        this.title = "";
+      }
+    }else{
+      console.error("in this class you have to use an object");
     }
   }
 }
