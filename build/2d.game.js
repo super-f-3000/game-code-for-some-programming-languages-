a63eba13c590d1eaebda73af63012c7fe9209566
super-f-3000 <81982904+super-f-3000@users.noreply.github.com>
2022-09-27 14:09:38 -0300

    Update 2d.game.js

--- build/2d.game.js
+++ build/2d.game.js
@@ -1,23 +1,6 @@
-class dialogLine {
-  constructor(parameters) {
-    if (typeof parameters === "object") {
-      if (parameters.title) {
-        this.title = parameters.title;
-      }else{
-        this.title = "";
-      }
-      if (parameters.texts) {
-        if (typeof parameters.texts === "object") {
-          this.texts = parameters.texts;
-        }else if (parameters.texts === undefined) {
-          this.texts = [];
-        }else{
-          console.error("parameters.texts has to be an array");
-          this.texts = [];
-        }
-      }
-    }else{
-      console.error("in this class you have to use an object");
-    }
+class Vector2 = {
+  constructor(x=0, y=0) {
+    this.x = x;
+    this.y = y;
   }
 }
